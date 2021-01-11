430899d329eaeb7469fc8e823acba8ec36ab57c3
DimaCh <chdmitr2@gmail.com>
2021-01-11 20:47:34 +0200

    Angular componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-
+export interface  Post {
+  title: string;
+  text: string;
+  id?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Init';
+   posts: Post[] = [
+     {title: 'I want to learn angular', text: 'I learn angular', id: 1},
+     {title: 'Next blog', text: 'About directives and pipes', id: 2}
+   ];
 
   // tslint:disable-next-line:typedef
-  onInput(event: any){
-    this.title = event.target.value;
-  }
+   updatePosts(post: Post){
+    // this.posts.unshift(post);
+   }
+
 }
