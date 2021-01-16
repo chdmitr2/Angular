ff493e42b464982010e5c03becfaa9292777a16e
DimaCh <chdmitr2@gmail.com>
2021-01-16 20:31:32 +0200

    start routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,11 @@
 
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {delay, subscribeOn} from 'rxjs/operators';
-import {Todo, TodoService} from './todo.service';
-
-
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  todos: Todo[] = [];
-  todoTitle = '';
-  loading = false;
-
-  constructor(private todoService: TodoService){
-
-  }
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
-     this.fetchTodos();
-  }
-
-  // tslint:disable-next-line:typedef
-  addTodo() {
-     if (!this.todoTitle.trim()){
-       return;
-     }
-
-     this.todoService.addTodo({
-       title: this.todoTitle,
-       completed: false
-     }).subscribe(todo => {
-       this.todos.push(todo);
-       this.todoTitle = '';
-     });
-
-  }
-
-  // tslint:disable-next-line:typedef
-  fetchTodos() {
-    this.loading = true;
-
-      this.todoService.fetchTodos()
-      // tslint:disable-next-line:align
-      .subscribe( todos => {
-        this.todos = todos;
-        this.loading = false;
-      });
-  }
-
-  // tslint:disable-next-line:typedef
-  removeTodo(id: number) {
-     this.todoService.removeTodo(id)
-     .subscribe(response => {
-             this.todos = this.todos.filter(t => t.id !== id);
-     });
-  }
-
-  // tslint:disable-next-line:typedef
-  completeTodo(id: number) {
-    this.todoService.completeTodo(id).subscribe(todo => {
-      console.log(todo);
-      this.todos.find(t => t.id === todo.id).completed = true;
-    });
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {PostsComponent} from './posts/posts.component';
+import {PostComponent} from './post/post.component';
+import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
